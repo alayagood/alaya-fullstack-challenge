a3dc5fd62a615e6dba4b1336b301e3af2fc29a8f
FMGordillo <facundomgordillo@gmail.com>
2022-06-20 16:23:32 +0200

    wip: added .env

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
+console.log(process.env.MONGODB_URL);
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mern-stack", {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
