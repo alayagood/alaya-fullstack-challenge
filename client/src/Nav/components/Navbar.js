5f4b7764ca50be6d789066e668925e5b4b805c6a
Sonia Meller <mellersonia@gmail.com>
2023-02-22 11:21:12 -0300

    client/src/Nav/components/Navbar.js: fix Link import

--- client/src/Nav/components/Navbar.js
+++ client/src/Nav/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
         <AppBar position="fixed">
             <Toolbar>
                 <Typography variant="h6" >
-                    <Link href="/" className="text-white">Home</Link>
+                    <Link to="/" className="text-white">Home</Link>
                 </Typography>
             </Toolbar>
         </AppBar>
