a018219b3dec38af2ac0853915d16e2c4c32ee85
Sonia Meller <mellersonia@gmail.com>
2023-02-22 13:38:19 -0300

    client/src/Post/components/: image upload ui updates

--- client/src/Post/components/PostListItem.js
+++ client/src/Post/components/PostListItem.js
@@ -5,11 +5,18 @@ import { Card } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 
 function PostListItem({ post, onDelete }) {
   return (
     <Card className="w-100 my-4">
+      {post.image && <CardMedia
+          component="img"
+          height="140"
+          image={post.image.src}
+          alt={post.image.label}
+      />}
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           <Link to={`/posts/${post.cuid}/${post.slug}`} >
