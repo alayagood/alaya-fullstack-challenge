65bcaa1e87bcc5b9b678c1bd6dd6bceb83acfabf
Sonia Meller <mellersonia@gmail.com>
2023-02-22 13:38:19 -0300

    client/src/Post/components/: image upload ui updates

--- client/src/Post/components/PostCreateWidget.js
+++ client/src/Post/components/PostCreateWidget.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import {imageUploader} from "../../util/imageUploader";
 import {useSelector} from "react-redux";
 import {getUserId} from "../../Auth/AuthReducer";
 
@@ -18,6 +19,8 @@ const PostCreateWidget = ({ addPost }) => {
 
     const [state, setState] = useState({});
     const [error, setError] = useState(undefined);
+    const [image, setImage] = useState(undefined);
+    const [loading, setLoading] = useState(false);
     const classes = useStyles();
 
     const userId = useSelector(getUserId, undefined);
@@ -28,6 +31,18 @@ const PostCreateWidget = ({ addPost }) => {
     const submit = async () => {
         if (!isValid) {
             setError(defaultError);
+            return;
+        }
+        if (image) {
+            setLoading(true);
+            const imgUploaderResp = await imageUploader(image)
+            if (!imgUploaderResp) {
+                setError(defaultError);
+                setLoading(false)
+                return;
+            }
+            addPost({...state, image: imgUploaderResp});
+            setLoading(false)
             return;
         }
         addPost(state);
@@ -41,15 +56,27 @@ const PostCreateWidget = ({ addPost }) => {
     });
   };
 
+    const handleFileChange = (evt) => {
+        setImage(evt.target.files[0])
+    }
+
   return (
     <div className={`${classes.root} d-flex flex-column my-4 w-100`}>
         <h3>Create new post</h3>
         <TextField variant="filled" label="Author name" name="name" onChange={handleChange} />
         <TextField variant="filled" label="Post title" name="title" onChange={handleChange} />
         <TextField variant="filled" multiline rows="4" label="Post content" name="content" onChange={handleChange} />
-        <Button className="mt-4" variant="contained" color="primary" onClick={() => submit()} disabled={!isValid}>
-            Submit
+        <TextField type="file" name="image" onChange={handleFileChange} />
+        <Button
+            className="mt-4"
+            variant="contained"
+            color="primary"
+            onClick={() => submit()}
+            disabled={!isValid || loading}
+        >
+            {loading? 'Loading...' : 'Submit'}
         </Button>
+        {error && <p>{error}</p>}
     </div>
   );
 };
--- client/src/Post/components/PostListItem.js
+++ client/src/Post/components/PostListItem.js
@@ -5,6 +5,7 @@ import { Card } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import {useSelector} from "react-redux";
 import {getUserId} from "../../Auth/AuthReducer";
@@ -18,6 +19,12 @@ function PostListItem({ post, onDelete }) {
 
   return (
     <Card className="w-100 my-4">
+      {post.image && <CardMedia
+          component="img"
+          height="140"
+          image={post.image.src}
+          alt={post.image.label}
+      />}
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           <Link to={getPostPageUrl(post)} >
