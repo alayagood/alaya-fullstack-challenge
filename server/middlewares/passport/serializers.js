d3633d03cc8aa54185ff0605bf2826f9625273d9
Sonia Meller <mellersonia@gmail.com>
2023-02-23 10:11:14 -0300

    serializers.js: deleted unused lines

--- server/middlewares/passport/serializers.js
+++ server/middlewares/passport/serializers.js
@@ -1,17 +1,16 @@
 const passport = require('passport')
 const User = require('../../models/user');
 
-// TODO - fix this section
 passport.serializeUser((loggedInUser, cb) => {
   cb(null, loggedInUser._id)
 })
 
 passport.deserializeUser((userIdFromSession, cb) => {
- const user =  User.findById(userIdFromSession)
-    .then(userDocument => {
-      cb(null, userDocument)
-    })
-    .catch(err => {
-      cb(err)
-    })
+    User.findById(userIdFromSession)
+        .then(userDocument => {
+            cb(null, userDocument)
+        })
+        .catch(err => {
+            cb(err)
+        })
 })
