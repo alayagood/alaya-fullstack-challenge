c6b5ce57f31ed2cf584f0544cbc3fce701c9e936
wolfee <wolfee.dm@gmail.com>
2023-02-25 16:51:07 +0100

    remove unnecessary log

--- client/src/util/apiCaller.js
+++ client/src/util/apiCaller.js
@@ -3,7 +3,6 @@ import fetch from 'isomorphic-fetch';
 export const API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 const call = async (endpoint, method = 'get', body) => {
-  console.log(API_URL);
   return fetch(`${API_URL}/${endpoint}`, {
     headers: { 'content-type': 'application/json' },
     method,
