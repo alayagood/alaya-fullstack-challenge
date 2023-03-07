ef2081992ff6816b3f87990dfc1fd8e1fc9084ed
ricardoribas <ricardo@dodo.eco>
2023-03-07 15:48:13 +0000

    Added upload logic WIP with errors while uploading file

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const PostController = require('../controllers/post.controller');
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 router.route('/posts').get(PostController.getPosts);
 router.route('/posts/:cuid').get(PostController.getPost);
+router
+	.route('/posts/:cuid/upload')
+	.post(upload.single('file'), PostController.uploadPostPhoto);
 router.route('/posts').post(PostController.addPost);
 router.route('/posts/:cuid').delete(PostController.deletePost);
 
