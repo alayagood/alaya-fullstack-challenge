68c4b6d66b6e551fe3f84e8d2e382915f5e7cad3
Pedro Simões <pedrosimoes93@gmail.com>
2023-03-11 16:43:58 +0000

    fix indentation styling

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-    .catch(e => {
-        console.error('Connection error', e.message);
-    });
+	.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+	.catch(e => {
+		console.error('Connection error', e.message);
+	});
 
 const db = mongoose.connection;
 
