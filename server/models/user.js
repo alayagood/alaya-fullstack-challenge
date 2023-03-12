02a08e9a51ca498ab546a5771b99e7362362a325
Pedro Simões <pedrosimoes93@gmail.com>
2023-03-12 23:23:32 +0000

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
 	accountName: { type: 'String', required: true, unique: true },
 	email: { type: 'String', required: true, unique: true },
 	password: { type: 'String', required: true },
+	salt: { type: 'String', required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
