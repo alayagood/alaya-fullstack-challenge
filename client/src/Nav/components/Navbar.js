fd712d140e8c611284f87e4a38e296a96f3e8ea7
Pedro Simões <pedrosimoes93@gmail.com>
2023-03-16 10:59:19 +0000

    use RR NavLink instead of Material UI's Link
    Material UI's Link was causing a whole rerender of the page

--- client/src/Nav/components/Navbar.js
+++ client/src/Nav/components/Navbar.js
@@ -3,14 +3,18 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { NavLink } from 'react-router-dom';
 
 function Navbar({ isLoggedIn }) {
     return (
         <AppBar position="fixed">
             <Toolbar>
                 <Typography variant="h6" >
-                    <Link href="/" className="text-white mr-4">Home</Link>
-                    <Link href="/auth" className="text-white">{isLoggedIn ? "Logout" : "Login / Register"}</Link>
+                    <NavLink to="/" className="text-white mr-4">Home</NavLink>
+                    { isLoggedIn ?
+                        <NavLink to="/logout" className="text-white">Logout</NavLink> :
+                        <NavLink to="/auth" className="text-white">Login / Register</NavLink>
+                    }
                 </Typography>
             </Toolbar>
         </AppBar>
