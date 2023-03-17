5c946525bd5095588193f7ee2c896a3d09907dac
Leticia Jyo <leticia.jyo@gmail.com>
2023-03-17 22:15:54 +0100

    implement jwt-passport

---
+++ server/auth/auth.js
@@ -0,0 +1,74 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const bcrypt = require('bcrypt');
+const UserModel = require('../models/user');
+const authConfig = require('../config/auth.config');
+var LoginError = require('../error/login.error');
+
+passport.use(
+  'signup',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const user = await UserModel.create({ email, password: hashedPassword });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(new LoginError('Invalid email or password'));
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(new LoginError('Invalid email or password'));
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: authConfig.jwt_secret,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
