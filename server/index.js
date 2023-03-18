c5aaaf0df77ab378948380a028cbaa2c036bf546
Leticia Jyo <leticia.jyo@gmail.com>
2023-03-18 20:54:02 +0100

    add image upload middleware

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const apiPort = 3000;
-const db = require('./db');
-const posts = require('./routes/post.routes');
-const auth = require('./routes/auth.routes');
+const db = require("./db");
+const posts = require("./routes/post.routes");
+const auth = require("./routes/auth.routes");
 
-require('./auth/auth');
+require("./middleware/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api', posts);
-app.use('/api', auth);
+app.use("/api", posts);
+app.use("/api", auth);
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
