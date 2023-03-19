d8eae389bd351d6274d4d1977b1e59022c1c6ec8
Leticia Jyo <leticia.jyo@gmail.com>
2023-03-19 20:24:53 +0100

    add email validation

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,57 +1,61 @@
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.config');
-const errorHandler = require('../error/auth.error_handler');
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.config");
+const errorHandler = require("../error/auth.error_handler");
+var emailValidator = require("email-validator");
 
 signUp = async (req, res, next) => {
-  validateAuthInput(req, res)
-  
+  if (!isValidAuthInput(req, res)) {
+    return;
+  }
+
   passport.authenticate(
-    'signup',
+    "signup",
     { session: false },
     async (err, user, next) => {
-      errorHandler.handleSignUpError(err, res)
-      
-      res.status(200).json({ success: true })
+      errorHandler.handleSignUpError(err, res);
+
+      res.status(200).json({ success: true });
     }
   )(req, res, next);
-}
+};
 
 login = async (req, res, next) => {
-  validateAuthInput(req, res)
-  
-  passport.authenticate(
-    'login',
-    async (err, user, next) => {
-      errorHandler.handleLoginError(err, user, res)
-
-      req.login(
-        user,
-        { session: false },
-        async (error) => {
-          if (error) return res.status(500).end()
-
-          const body = { _id: user._id, email: user.email };
-          const token = jwt.sign(
-            { user: body }, 
-            authConfig.jwt_secret, 
-            { expiresIn: "2h" }
-          );
-
-          return res.status(200).json({ token });
-        }
-      );
-    }
-  )(req, res, next);
-}
-
-validateAuthInput = (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).json({ message: 'Email and password must be provided' })
+  if (!isValidAuthInput(req, res)) {
+    return;
   }
-}
+
+  passport.authenticate("login", async (err, user, next) => {
+    errorHandler.handleLoginError(err, user, res);
+
+    req.login(user, { session: false }, async (error) => {
+      if (error) return res.status(500).end();
+
+      const body = { _id: user._id, email: user.email };
+      const token = jwt.sign({ user: body }, authConfig.jwt_secret, {
+        expiresIn: "2h",
+      });
+
+      return res.status(200).json({ token });
+    });
+  })(req, res, next);
+};
+
+isValidAuthInput = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).json({ message: "Email and password must be provided" });
+    return false;
+  }
+
+  if (!emailValidator.validate(req.body.email)) {
+    res.status(400).json({ message: "Invalid email" });
+    return false;
+  }
+
+  return true;
+};
 
 module.exports = {
   signUp,
-  login
+  login,
 };
