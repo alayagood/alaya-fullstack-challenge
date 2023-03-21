cb737905f1669193f056c1cddda1e6dadb5b3ed2
Luis Cabello <luis.cab.fer@gmail.com>
2023-03-21 23:41:39 +0100

    challenge(feat): improve navbar

--- client/src/Nav/components/Navbar.js
+++ client/src/Nav/components/Navbar.js
@@ -3,13 +3,19 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import { useSelector } from 'react-redux';
 
 function Navbar() {
+    const isAuthenticated = useSelector((state) => state.user);
+
     return (
         <AppBar position="fixed">
             <Toolbar>
                 <Typography variant="h6" >
-                    <Link href="/" className="text-white">Home</Link>
+                    { isAuthenticated
+                        ? <Link href="/" className="text-white">Home</Link>
+                        : <Link href="/login" className="text-white">Login</Link>
+                    }
                 </Typography>
             </Toolbar>
         </AppBar>
