6e6c4dec65a1457afdfd99dfd96f7a12b5c84782
dmmz <dmartinmartinez@gmail.com>
2023-05-01 10:05:19 +0200

    fix: initial test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {render} from "@testing-library/react";
+import configureMockStore from "redux-mock-store";
+import App from "./App";
+import {fetchPosts} from "./Post/PostActions";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+jest.mock("./Post/PostActions", () => ({
+  fetchPosts: jest.fn(),
+}));
+
+test("renders initial page", () => {
+  const initialState = {
+    posts: {data: []},
+  };
+  fetchPosts.mockReturnValue({type: "FETCH_POSTS"});
+
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+  const {getByText} = render(<App store={store} />);
+  const linkElement = getByText(/alaya blog/i);
   expect(linkElement).toBeInTheDocument();
 });
--- client/src/Post/pages/PostListPage/PostListPage.js
+++ client/src/Post/pages/PostListPage/PostListPage.js
@@ -1,24 +1,24 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {useEffect} from "react";
+import PropTypes from "prop-types";
+import {useDispatch, useSelector} from "react-redux";
 // Import Components
-import PostList from '../../components/PostList';
-import PostCreateWidget from '../../components/PostCreateWidget';
+import PostList from "../../components/PostList";
+import PostCreateWidget from "../../components/PostCreateWidget";
 // Import Actions
-import { addPostRequest, deletePostRequest, fetchPosts } from '../../PostActions';
-import Logo from '../../../logo.svg';
+import {addPostRequest, deletePostRequest, fetchPosts} from "../../PostActions";
+import Logo from "../../../logo.svg";
 
-const PostListPage = ({ showAddPost }) => {
-
+const PostListPage = ({showAddPost}) => {
   const dispatch = useDispatch();
-  const posts = useSelector(state => state.posts.data);
+  const posts = useSelector((state) => state.posts.data);
 
   useEffect(() => {
     dispatch(fetchPosts());
-  },[]);
+  }, []);
 
-  const handleDeletePost = post => {
-    if (confirm('Do you want to delete this post')) { // eslint-disable-line
+  const handleDeletePost = (post) => {
+    // eslint-disable-next-line
+    if (confirm("Do you want to delete this post")) {
       dispatch(deletePostRequest(post));
     }
   };
@@ -31,10 +31,13 @@ const PostListPage = ({ showAddPost }) => {
     <div className="container">
       <div className="row">
         <div className="col-12 d-flex align-items-center">
-          <img className="mx-3" src={Logo} alt="Logo" style={{ height: '72px'}}/>
-          <h1 className="mt-4">
-             Alaya Blog
-          </h1>
+          <img
+            className="mx-3"
+            src={Logo}
+            alt="Logo"
+            style={{height: "72px"}}
+          />
+          <h1 className="mt-4">Alaya Blog</h1>
         </div>
       </div>
       <hr />
@@ -51,8 +54,7 @@ const PostListPage = ({ showAddPost }) => {
 };
 
 PostListPage.propTypes = {
-  showAddPost: PropTypes.bool.isRequired
+  // showAddPost: PropTypes.bool.isRequired,
 };
-
 
 export default PostListPage;
