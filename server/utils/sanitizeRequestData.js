560d655d2b0bf18b83091bb847b11bbffdaf69e7
alextota <alextorrestara@gmail.com>
2023-05-05 17:22:12 +0200

    improve sanitize function
    update sanitizeRequestData() to recursively iterate through all props of the object

--- server/utils/sanitizeRequestData.js
+++ server/utils/sanitizeRequestData.js
@@ -1,12 +1,16 @@
 const sanitizeHtml = require('sanitize-html');
 
 /**
- * Iterates through all props of an object and sanitizes their values
+ * Recursively iterates through all props of an object and sanitizes their values
  */
 module.exports = function(data) {
     for (const property in data) {
-        const sanitizedValue = sanitizeHtml(data[property]);
-        data[property] = sanitizedValue;
+        if (typeof data[property] === 'object') {
+            data[property] = module.exports(data[property]);
+        } else {
+            const sanitizedValue = sanitizeHtml(data[property]);
+            data[property] = sanitizedValue;
+        }
     }
     return data;
 }
