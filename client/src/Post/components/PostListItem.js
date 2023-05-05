5d10796c34da72bd62d2d5e87023c3ecd674131c
dmmz <dmartinmartinez@gmail.com>
2023-05-05 14:51:09 +0200

    feat: show only delete link for current users posts

--- client/src/Post/components/PostListItem.js
+++ client/src/Post/components/PostListItem.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Card } from '@material-ui/core';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import {Card} from "@material-ui/core";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
-function PostListItem({ post, onDelete }) {
+function PostListItem({post, onDelete, showDeleteLink}) {
   return (
     <Card className="w-100 my-4">
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
-          <Link to={`/posts/${post.cuid}/${post.slug}`} >
-            {post.title}
-          </Link>
+          <Link to={`/posts/${post.cuid}/${post.slug}`}>{post.title}</Link>
         </Typography>
         <Typography component="p" className="mt-3">
           {post.content}
         </Typography>
-        <Typography color="textSecondary" component="p" className="mt-3 font-italic">
+        <Typography
+          color="textSecondary"
+          component="p"
+          className="mt-3 font-italic"
+        >
           From {post.name}
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary" onClick={onDelete}>
-          Delete post
-        </Button>
-      </CardActions>
+      {showDeleteLink && (
+        <CardActions>
+          <Button size="small" color="secondary" onClick={onDelete}>
+            Delete post
+          </Button>
+        </CardActions>
+      )}
     </Card>
   );
 }
@@ -41,6 +45,7 @@ PostListItem.propTypes = {
     cuid: PropTypes.string.isRequired,
   }).isRequired,
   onDelete: PropTypes.func.isRequired,
+  showDeleteLink: PropTypes.bool.isRequired,
 };
 
 export default PostListItem;
