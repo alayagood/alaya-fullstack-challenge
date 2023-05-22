38758fc256470b84f6113006193e0866934866d8
Jake Broughton <jake@mythical.fish>
2023-05-22 13:22:13 +0200

    Check token expiry

--- client/src/util/token.js
+++ client/src/util/token.js
@@ -6,12 +6,24 @@ export const getToken = () => Cookies.get(TOKEN_KEY);
 export const setToken = (value) => Cookies.set(TOKEN_KEY, value);
 export const unsetToken = () => Cookies.remove(TOKEN_KEY);
 
-export const getUserInfo = (jwt) => {
-  const token = jwt || getToken();
+export const getTokenPayload = (token) => {
   if (!token) return undefined;
   let payload = token.split(".")[1];
   if (!payload) return undefined;
   payload = atob(payload);
-  const info = JSON.parse(payload);
-  return { id: info.id, username: info.username };
+  return JSON.parse(payload);
+};
+
+export const getUserInfo = (jwt) => {
+  const token = jwt || getToken();
+  if (!token) return null;
+  if (isTokenExpired(token)) return null;
+  const data = getTokenPayload(token);
+  return { id: data.id, username: data.username };
+};
+
+export const isTokenExpired = (token) => {
+  const data = getTokenPayload(token);
+  const expireTime = data.exp * 1000;
+  return Date.now() >= expireTime;
 };
