16c47713bfdea664ffdb54d4a8528dfc50fccf97
Jake Broughton <jake@mythical.fish>
2023-05-22 13:51:00 +0200

    Add Bearer, add protected API endpoints

--- client/src/util/apiCaller.js
+++ client/src/util/apiCaller.js
@@ -1,23 +1,28 @@
-import fetch from 'isomorphic-fetch';
+import fetch from "isomorphic-fetch";
+import { getToken } from "./token";
 
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = "http://localhost:3000/api";
 
-export default async (endpoint, method = 'get', body) => {
+export default async (endpoint, method = "get", body) => {
+  const token = getToken();
+  const headers = { "content-type": "application/json" };
+  if (token) headers["Authorization"] = `Bearer ${token}`;
+
   return fetch(`${API_URL}/${endpoint}`, {
-    headers: { 'content-type': 'application/json' },
+    headers,
     method,
     body: JSON.stringify(body),
   })
-  .then(response => response.json().then(json => ({ json, response })))
-  .then(({ json, response }) => {
-    if (!response.ok) {
-      return Promise.reject(json);
-    }
+    .then((response) => response.json().then((json) => ({ json, response })))
+    .then(({ json, response }) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
 
-    return json;
-  })
-  .then(
-    response => response,
-    error => error
-  );
-}
+      return json;
+    })
+    .then(
+      (response) => response,
+      (error) => error
+    );
+};
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const PostController = require('../controllers/post.controller');
+const PostController = require("../controllers/post.controller");
+const { authenticateToken } = require("./middleware");
 
 // Get all Posts
-router.route('/posts').get(PostController.getPosts);
+router.route("/posts").get(authenticateToken, PostController.getPosts);
 
 // Get one post by cuid
-router.route('/posts/:cuid').get(PostController.getPost);
+router.route("/posts/:cuid").get(authenticateToken, PostController.getPost);
 
 // Add a new Post
-router.route('/posts').post(PostController.addPost);
+router.route("/posts").post(authenticateToken, PostController.addPost);
 
 // Delete a post by cuid
-router.route('/posts/:cuid').delete(PostController.deletePost);
+router
+  .route("/posts/:cuid")
+  .delete(authenticateToken, PostController.deletePost);
 
 module.exports = router;
