69c40bf32afff338d5f5472341522ca1e4d929bc
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2023-09-13 11:44:45 +0200

    (SERVER) Add user integration tests

---
+++ server/src/__tests__/integration/user.integration.test.ts
@@ -0,0 +1,83 @@
+import request from 'supertest';
+import '../../db'; // Import your database connection
+import User from '../../models/user'; // I'm assuming you have a user model
+import bcrypt from 'bcryptjs';
+import { waitForServerToStart } from './helpers';
+
+const baseURL = 'http://localhost:5000/api';
+
+describe('User Routes', () => {
+  beforeAll(async () => {
+    await waitForServerToStart(baseURL, 30000);
+  })
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  describe('Login Tests', () => {
+    it('should fail with invalid data', async () => {
+      const response = await request(baseURL).post('/user/login').send({ email: 'invalid', password: 'password' });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.detail).toBe('Invalid User Data');
+    });
+
+    it('should fail with non-existing user', async () => {
+      const response = await request(baseURL).post('/user/login').send({ email: 'nonexistent@example.com', password: 'password123' });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.detail).toBe('Login Failed');
+    });
+
+    it('should fail with correct user but wrong password', async () => {
+      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      await user.save();
+      const response = await request(baseURL).post('/user/login').send({ email: 'test@example.com', password: 'wrongPassword' });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.detail).toBe('Login Failed');
+    });
+
+    it('should succeed with correct user and password', async () => {
+      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      await user.save();
+      const response = await request(baseURL).post('/user/login').send({ email: 'test@example.com', password: 'correctPassword' });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.accessToken).toBeTruthy();
+      expect(response.body.user.email).toBe('test@example.com');
+    });
+    it('should not return password', async () => {
+      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      await user.save();
+      const response = await request(baseURL).post('/user/login').send({ email: 'test@example.com', password: 'correctPassword' });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.accessToken).toBeTruthy();
+      expect(response.body.password).toBe(undefined);
+    });
+  });
+
+  describe('Signup Tests', () => {
+    it('should fail with invalid data', async () => {
+      const response = await request(baseURL).post('/user/signup').send({ email: 'invalid', password: 'short' });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.detail).toBe('Invalid User Data');
+    });
+
+    it('should succeed with valid data', async () => {
+      const response = await request(baseURL).post('/user/signup').send({ email: 'newuser@example.com', password: 'newpassword123' });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.user.email).toBe('newuser@example.com');
+    });
+    it('should not return password', async () => {
+      const response = await request(baseURL).post('/user/signup').send({ email: 'newuser2@example.com', password: 'newpassword123' });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.password).toBe(undefined);
+    });
+
+    it('should fail with existing email', async () => {
+      const user = new User({ email: 'existing@example.com', password: bcrypt.hashSync('password123', 10) });
+      await user.save();
+      const response = await request(baseURL).post('/user/signup').send({ email: 'existing@example.com', password: 'password123' });
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe('User already exists');
+    });
+  });
+});
+
