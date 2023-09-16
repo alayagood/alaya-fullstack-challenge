7671a941ed7972d165b5bbf7667df42f03df7da0
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2023-09-16 16:40:33 +0200

    (CLIENT) Add Authorization Headers to apiCall

--- client/src/util/apiCaller.js
+++ client/src/util/apiCaller.js
@@ -3,21 +3,28 @@ import fetch from 'isomorphic-fetch';
 export const API_URL = 'http://localhost:3000/api';
 
 export default async (endpoint, method = 'get', body) => {
+
+  const headers = { 'content-type': 'application/json' }
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+
   return fetch(`${API_URL}/${endpoint}`, {
-    headers: { 'content-type': 'application/json' },
+    headers,
     method,
     body: JSON.stringify(body),
   })
-  .then(response => response.json().then(json => ({ json, response })))
-  .then(({ json, response }) => {
-    if (!response.ok) {
-      return Promise.reject(json);
-    }
+    .then(response => response.json().then(json => ({ json, response })))
+    .then(({ json, response }) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
 
-    return json;
-  })
-  .then(
-    response => response,
-    error => error
-  );
+      return json;
+    })
+    .then(
+      response => response,
+      error => error
+    );
 }
