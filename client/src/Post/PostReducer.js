077b58941d7713c64ba602a6d4529a8b402c948a
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2023-09-16 16:41:30 +0200

    (CLIENT) Fix Post reducer and post list errors

--- client/src/Post/PostReducer.js
+++ client/src/Post/PostReducer.js
@@ -5,17 +5,20 @@ const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST :
-      return {
-        data: [action.post, ...state.data],
-      };
+    case ADD_POST:
+      if (action.post) {
+        return {
+          data: [action.post, ...state.data],
+        };
+      }
+      return state
 
-    case ADD_POSTS :
+    case ADD_POSTS:
       return {
         data: action.posts,
       };
 
-    case DELETE_POST :
+    case DELETE_POST:
       return {
         data: state.data.filter(post => post.cuid !== action.cuid),
       };
