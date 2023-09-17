f63194c0fa8c4c87444b1238c263212834eddb45
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2023-09-17 16:38:53 +0200

    (SERVER) Add cloudinary config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,6 +1,5 @@
 const pe = process.env;
 
-
 export const MONGO_URI: string = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mern-stack';
 
 export const PORT: number = Number(process.env.PORT) || 3000;
@@ -10,13 +9,17 @@ export const ENVIRONMENT = (pe.ENVIRONMENT as Environment) || 'development';
 
 export const SECRET_KEY = pe.SECRET_KET || "donttellanyone please"
 
-if (ENVIRONMENT==="production" && !pe.SECRET_KEY) {
+if (ENVIRONMENT === "production" && !pe.SECRET_KEY) {
   throw new Error('Please provide SECRET_KEY environment variable!');
 }
 
-export const JWT_ACCESS_TOKEN_LIFETIME = pe.JWT_ACCESS_TOKEN_LIFETIME || '1h';
+export const JWT_ACCESS_TOKEN_LIFETIME = pe.JWT_ACCESS_TOKEN_LIFETIME || '2d';
 
 // Client
+
+export const CLOUDINARY_CLOUD_NAME = pe.CLOUDINARY_CLOUD_NAME
+export const CLOUDINARY_API_KEY = pe.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_SECRET = pe.CLOUDINARY_API_SECRET
 
 export const CLIENT_ORIGIN = pe.CLIENT_ORIGIN || `http://localhost:8000`;
 
