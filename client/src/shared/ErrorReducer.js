0d806dbb774b6428447676bb415241675c9a5ed8
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2023-09-19 10:23:29 +0200

    (CLIENT) fix error reducer

--- client/src/shared/ErrorReducer.js
+++ client/src/shared/ErrorReducer.js
@@ -4,17 +4,20 @@ const initialState = {
   message: null,
 };
 export const CLEAR_ERROR = "CLEAR_ERROR"
-
+export const HTTP_ERROR = "HTTP_ERROR"
 
 const errorReducer = (state = initialState, action) => {
+  if (action.type === CLEAR_ERROR) {
+    return initialState;
+  }
+
   if (action.type.endsWith('_ERROR')) {
     return {
       error: action.error,
       message: action.message
     };
-  } else if (action.type === 'CLEAR_ERROR') {
-    return initialState;
   }
+
   return state;
 };
 
